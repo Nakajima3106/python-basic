4a2f1e80b8896d4963ada68d19ff1b669ad94c2c
Nakajima3106 <uver777.333@gmail.com>
2021-02-23 13:58:56 +0000

    2章終了

---
+++ variable.py
@@ -0,0 +1,9 @@
+first_name = "花子"
+last_name = "山田"
+print(last_name + first_name)
+
+last_name = "鈴木"
+print(last_name + first_name)
+
+middle_name = "キャサリン"
+print(last_name + middle_name + first_name)
